06b83e24f8f421ca7ff99711b4f63ad1794ec121
maricicf <127163403+maricicf@users.noreply.github.com>
2026-05-25 16:53:23 +0200

    fix: add CostCenter tag in rt-association

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -62,4 +62,9 @@ resource "aws_route_table" "public" {
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
+
+   tags = {
+    Name       = "public-rt-association"
+    CostCenter = "123456"
+  }
 }
